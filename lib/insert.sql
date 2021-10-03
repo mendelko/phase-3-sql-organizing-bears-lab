00ad2d9790ba18f973778697ad2be89ab71233fe
Mendel <mkonikov123@gmail.com>
2021-10-03 13:09:07 -0400

    commit tree c5a3a4cd04c1aecd7b2cb3e8905af94d8cd3e973

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'angry', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'black', 'hyper', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'blond', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'red', 'ecstatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 11, 'F', 'orange', 'shy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Black', 'Grinchy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'Blue', 'naive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, 'M', 'black', 'aggressive', 0);
